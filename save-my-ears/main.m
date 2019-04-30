ad67817d41bb9068c7a3272ae7f640a9ee97d1c9
Arunvel Sriram <arunvelsriram@gmail.com>
2019-04-30 22:40:05 +0530

    Detect jack insertion and set volume to 25%

--- save-my-ears/main.m
+++ save-my-ears/main.m
@@ -7,11 +7,74 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreAudio/CoreAudio.h>
+
+UInt32 activeDataSource(AudioDeviceID deviceId, AudioObjectPropertyAddress dataSourceAddr) {
+    UInt32 dataSourceId = 0;
+    UInt32 dataSourceIdSize = sizeof(UInt32);
+    AudioObjectGetPropertyData(deviceId, &dataSourceAddr, 0, NULL, &dataSourceIdSize, &dataSourceId);
+    
+    return dataSourceId;
+}
+
+void setVolume(AudioDeviceID deviceId, Float32 volume) {
+    UInt32 volumeSize = sizeof(Float32);
+    OSStatus status;
+    
+    AudioObjectPropertyAddress leftVolumePropertyAddr = {
+        kAudioDevicePropertyVolumeScalar,
+        kAudioDevicePropertyScopeOutput,
+        1 /*LEFT_CHANNEL*/
+    };
+    
+    AudioObjectPropertyAddress rightVolumePropertyAddr = {
+        kAudioDevicePropertyVolumeScalar,
+        kAudioDevicePropertyScopeOutput,
+        2 /*RIGHT_CHANNEL*/
+    };
+    
+    status = AudioObjectSetPropertyData(deviceId, &leftVolumePropertyAddr, 0, NULL, volumeSize, &volume);
+    if (status != kAudioHardwareNoError) {
+        printf("Failed to set left channel volume. OSStatus: %d\n", status);
+    }
+    
+    status = AudioObjectSetPropertyData(deviceId, &rightVolumePropertyAddr, 0, NULL, volumeSize, &volume);
+    if (status != kAudioHardwareNoError) {
+        printf("Failed to set right channel volume. OSStatus: %d\n", status);
+    }
+    
+    printf("Set volume to %.0f%%\n", (volume * 100));
+}
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
-    return 0;
+    const Float32 DEFAULT_VOLUME = 0.25;
+    
+    // Get default output device address
+    AudioDeviceID defaultDevice = 0;
+    UInt32 defaultSize = sizeof(AudioDeviceID);
+    const AudioObjectPropertyAddress defaultDeviceAddr = {
+        kAudioHardwarePropertyDefaultOutputDevice,
+        kAudioObjectPropertyScopeGlobal,
+        kAudioObjectPropertyElementMaster
+    };
+    AudioObjectGetPropertyData(kAudioObjectSystemObject, &defaultDeviceAddr, 0, NULL, &defaultSize, &defaultDevice);
+    
+    AudioObjectPropertyAddress dataSourceAddr;
+    dataSourceAddr.mSelector = kAudioDevicePropertyDataSource;
+    dataSourceAddr.mScope = kAudioDevicePropertyScopeOutput;
+    dataSourceAddr.mElement = kAudioObjectPropertyElementMaster;
+    
+    // Listen to changes in the device
+    AudioObjectAddPropertyListenerBlock(defaultDevice, &dataSourceAddr, nil, ^(UInt32 inNumberAddr, const AudioObjectPropertyAddress *inAddr) {
+        // Get the active data source
+        UInt32 activeDataSourceId = activeDataSource(defaultDevice, dataSourceAddr);
+        if (activeDataSourceId == 'ispk') {
+            printf("Internal speaker detected. DataSourceID: %d\n", activeDataSourceId);
+        } else if (activeDataSourceId == 'hdpn') {
+            printf("Headphone/External speaker detected. DataSourceID: %d\n", activeDataSourceId);
+            setVolume(defaultDevice, DEFAULT_VOLUME);
+        }
+    });
+    
+    CFRunLoopRun();
 }

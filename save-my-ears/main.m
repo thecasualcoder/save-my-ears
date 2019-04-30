f092e02f3339c4ef5559f1f99823832a7ec0876f
Arunvel Sriram <arunvelsriram@gmail.com>
2019-04-30 22:07:22 +0530

    Initial Commit

---
+++ save-my-ears/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  save-my-ears
+//
+//  Created by Arunvel Sriram on 30/04/19.
+//  Copyright © 2019 Arunvel Sriram. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
